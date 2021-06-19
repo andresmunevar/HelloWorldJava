a383ba25a64af6e0991567ea3ec9978f24ae85c0
PipeMun <afmunene@gmail.com>
2021-06-19 08:43:47 -0300

    first commit

---
+++ HolaMundo.java
@@ -0,0 +1,18 @@
+public class HolaMundo {
+  public static void main(String args[]){
+    System.out.println("Hello World from Java");
+    int myIntVariable = 10;
+    System.out.println(myIntVariable);
+    myIntVariable = 5;
+    System.out.println(myIntVariable);
+    String myStringVariable = "Hi";
+    System.out.println(myStringVariable);
+    myStringVariable = "Bye";
+    System.out.println(myStringVariable);
+    var myIntVariable2 = 15;
+    System.out.println(myIntVariable2);
+    var myStringVariable2 = "Hello Hello";
+    System.out.println(myStringVariable2);
+
+  }
+}
